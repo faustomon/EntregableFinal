ddb75cc6ef46556430b83e723746ce2fea2f188b
faustomon <105680262+faustomon@users.noreply.github.com>
2022-06-05 12:40:31 -0300

    Se agrego el esceleto del template (no funciona el static)

--- EntregableFinal/AppEntregable/urls.py
+++ EntregableFinal/AppEntregable/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from AppEntregable import views
 from AppEntregable.views import *
 urlpatterns = [
-    path("", inicio, name="Inicio")
+    path("", inicio, name="Inicio"),
 ] 
